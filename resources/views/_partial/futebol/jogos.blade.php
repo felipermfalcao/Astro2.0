4634b230bee0760043e387e73731d20ddc9097ff
felipefalcao <contato@felipefalcao.com.br>
2022-11-05 16:26:49 -0300

    Atualização da API Futebol

--- resources/views/_partial/futebol/jogos.blade.php
+++ resources/views/_partial/futebol/jogos.blade.php
@@ -7,10 +7,28 @@
      data-key="8567c6e5eade0e9932043fca48dc429c"
      data-theme="dark"
      data-show-errors="false"
-     class="api_football_loader"></div>
+     data-show-logos="true"
+     class="wg_loader"></div>
 
 <h1 style="font-size: 60px; padding-top: 50px;">Rodadas</h1>
-<div id="wg-api-football-fixtures"
+<div id="wg-api-football-games"
+     data-host="v3.football.api-sports.io"
+     data-key="8567c6e5eade0e9932043fca48dc429c"
+     data-date=""
+     data-league="{{$id}}"
+     data-season="{{isset($anoC) ? $anoC : $ano}}"
+     data-theme="dark"
+     data-refresh="0"
+     data-show-toolbar="true"
+     data-show-errors="false"
+     data-show-logos="true"
+     data-modal-game="true"
+     data-modal-standings="true"
+     data-modal-show-logos="true">
+</div>
+
+
+<!--<div id="wg-api-football-fixtures"
      data-host="v3.football.api-sports.io"
      data-refresh="0"
      data-date=""
@@ -23,9 +41,10 @@
      data-theme=""
      data-show-errors="false"
      class="api_football_loader">
-</div>
+</div>-->
 
+<!--https://widgets.api-sports.io/football/1.1.8/widget.js-->
 <script
     type="module"
-    src="https://widgets.api-sports.io/football/1.1.8/widget.js">
+    src="https://widgets.api-sports.io/2.0.3/widgets.js">
 </script>
